ee8400669fd6a52bc58b7245bed8e3ea4f1089d0
Your Name <you@example.com>
2019-10-23 13:21:21 +0800

    la

---
+++ .gitignore.py
@@ -0,0 +1,27 @@
+__pycache__/
+*.py[cod]
+*$py.class 
+/target/ 
+
+.classpath
+.project
+.settings
+ ##sln file##
+*.mdb  
+*.ldb  
+*.sln    
+##class file##
+*.class  
+*.dll   
+*.o  
+*.so  
+# compression file
+*.7z  
+*.dmg  
+*.gz  
+*.iso  
+*.jar   
+*.tar  
+*.via
+*.tmp
+*.err 
